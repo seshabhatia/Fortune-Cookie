c40ce1143234e50b11399dc71b901993175bdf78
Sesha Bhatia <sesha.bhatia@gmail.com>
2017-03-21 21:03:31 -0500

    Created Fortune Cookie assignment

--- main.py
+++ main.py
@@ -15,10 +15,15 @@
 # limitations under the License.
 #
 import webapp2
+import random
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        header = "<h1>Fortune Cookie</h1>"
+        lucky_number = random.randint(1,100)
+        number_sentence = "Your lucky number is: "+str(lucky_number)
+        number_paragraph = "<p>" + number_sentence + "</p>"
+        self.response.write(header + number_paragraph )
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
